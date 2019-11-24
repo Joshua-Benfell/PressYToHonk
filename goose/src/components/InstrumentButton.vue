dfe1f6f873fbe0fcb5c869500f7dcf1c037d4829
Joshua Benfell <joshuabenfell@hotmail.co.nz>
2019-11-24 23:36:14 +1300

    Got Sounds playing and Instruments changing

---
+++ goose/src/components/InstrumentButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="button instrumentButton" @click="emitToParent">
+    <img class='instrumentIcon' :src="imageLoc" :alt="name+'-icon'">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InstrumentButton',
+  props: ['icon', 'name', "active"],
+  data () {
+    return {
+      baseImgLoc: "/src/assets/img/"
+    }
+  },
+  computed:{
+    imageLoc(){
+      return this.baseImgLoc.concat(this.icon)
+    }
+  },
+  methods:{
+    emitToParent(event){
+      this.$emit('instrumentChange', this.name)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .instrumentButton{
+    width: 75px;
+    height: 75px;
+    margin: 10px auto;
+
+    .instrumentIcon{
+      max-width: 50px;
+      max-height: 50px;
+    }
+  }
+
+
+</style>
