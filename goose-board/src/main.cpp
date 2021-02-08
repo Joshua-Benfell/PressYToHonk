0da0049504208011841ebac1b5874e4ed7b28325
Josh Benfell <joshuabenfell@hotmail.co.nz>
2021-02-08 21:45:52 +1300

    Got it Honking!

--- goose-board/src/main.cpp
+++ goose-board/src/main.cpp
@@ -1,24 +1,37 @@
 #include <Arduino.h>
-#include <OpenSmartSerialMP3.h>
+// #include <OpenSmartSerialMP3.h>
 
 #define HW_SERIAL 2
 #define BAUD 9600
-
-MP3 mp3(BAUD);
+// https://www.theamplituhedron.com/articles/How-to-use-the-Serial-MP3-Player-UART-with-Speaker-by-OPEN-SMART-with-Arduino/
+// MP3 mp3(BAUD);
 
 enum folders_reference{
     honk = 1, glass, harmonica, soap, trash_lid, walkie_talkie
 } folders;
 
+uint8_t select_SD_card[] = {0x7e, 0x03, 0X35, 0x01, 0xef}; // 7E 03 35 01 EF
+uint8_t play_folder_honk[] = {0x7e, 0x04, 0x42, 0x01, 0x01, 0xef}; // 7E 04 42 01 01 EF
+uint8_t play_folder_glass[] = {0x7e, 0x04, 0x42, 0x02, 0x01, 0xef}; // 7E 04 42 02 01 EF
+
+void send_command_to_MP3_player(uint8_t command[], int len){
+    Serial.print("\nMP3 Command => ");
+    for(int i=0;i<len;i++){
+        Serial2.write(command[i]); 
+        Serial.print(command[i], HEX);
+    }
+    delay(1000);
+}
+
+
 void setup() {
-    delay(500);
-    mp3.setVolume(0x1e);
-    delay(50);
-    mp3.playWithFileName(0x01, 0x01);
-    delay(50);
-    Serial.println(mp3.getStatus());
+    Serial.begin(115200);
+    Serial2.begin(9600);
+    send_command_to_MP3_player(select_SD_card, 5);
+    send_command_to_MP3_player(play_folder_honk, 6);
+    delay(2000);
+    send_command_to_MP3_player(play_folder_glass, 6);
 }
 
 void loop() {
-
 }
